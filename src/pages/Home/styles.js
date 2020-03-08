cc673800b7e3066ccdcdc282b470374ad539d4a9
thiagofrancisquete <thiago.francisquete@gmail.com>
2020-03-07 22:34:45 -0300

    home page styles

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const ProductList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  list-style: none;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+    text-align: center;
+    text-transform: uppercase;
+
+    > span {
+      margin-bottom: 10px;
+    }
+
+    img {
+      align-self: center;
+      max-width: 250px;
+    }
+
+    > strong {
+      font-size: 16px;
+      line-height: 20px;
+      color: #333;
+      margin-top: 5px;
+    }
+
+    button {
+      background: #7159a1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      overflow: hidden;
+      margin-top: auto;
+
+      display: flex;
+      align-items: center;
+      transition: background .5s;
+
+      &:hover {
+        background: #7159c9;
+      }
+
+      div {
+        display: flex;
+        padding: 12px;
+        background: rgba(0, 0, 0, 0.1);
+        align-items: center;
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+  }
+`;
