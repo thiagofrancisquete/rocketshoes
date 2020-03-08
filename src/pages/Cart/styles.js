1c72218c445634f0ea0fc269cd8a94189278bb65
thiagofrancisquete <thiago.francisquete@gmail.com>
2020-03-07 22:34:21 -0300

    create cart styles

---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
+
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      background: #7159a1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+      transition: background 0.5s;
+
+      &:hover {
+        background: #7159c9;
+      }
+    }
+  }
+`;
+
+export const ProductTable = styled.table`
+  text-transform: uppercase;
+  width: 100%;
+
+  thead th {
+    color: #999;
+    text-align: left;
+    padding: 12px;
+  }
+
+  tbody td {
+    padding: 12px;
+    border-bottom: 1px solid #eee;
+  }
+
+  img {
+    height: 100px;
+  }
+
+  strong {
+    color: #333;
+    display: block;
+  }
+
+  span {
+    color: #7159a1;
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-self: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
+`;
+
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+  text-transform: uppercase;
+
+  span {
+    color: #999;
+    font-weight: bold;
+  }
+
+  strong {
+    color: #7159a1;
+    font-size: 20px;
+    margin-left: 5px;
+  }
+`;
