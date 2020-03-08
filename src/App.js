54da87e294a1f755aa01d1a2b07bc3975d66cdf3
thiagofrancisquete <thiago.francisquete@gmail.com>
2020-03-07 22:32:33 -0300

    import Heder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 import GlobalStyle from './styles/global';
-import Header from './components/Header';
+import Header from './components/Header/index';
 
 import './App.css';
 
