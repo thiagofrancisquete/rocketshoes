6091b2b59f41c53a4bea61d812eb81ffb0b80c68
thiagofrancisquete <thiago.francisquete@gmail.com>
2020-03-09 21:11:41 -0300

    change case name 2 be equal types @cart/add/remove

--- src/redux/modules/cart/reducer.js
+++ src/redux/modules/cart/reducer.js
@@ -2,7 +2,7 @@ import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
-    case 'ADD_TO_CART':
+    case '@cart/ADD':
       return produce(state, draft => {
         // verifica se o id produto no carrinho tem o id recebido na action
         const productIndex = draft.findIndex(p => p.id === action.product.id);
@@ -15,7 +15,7 @@ export default function cart(state = [], action) {
           draft.push({ ...action.product, amount: 1 });
         }
       });
-      case 'REMOVE_FROM_CART':
+      case '@cart/REMOVE':
         return produce(state, draft => {
           const productIndex = draft.findIndex(p => p.id === action.id);
           
